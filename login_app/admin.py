aaca3458482127adee73ae1a451b44b98a21bab5
Adoneg <victoradoneg2@gmail.com>
2023-09-02 10:32:10 +0100

    new folder

--- login_app/admin.py
+++ login_app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import User
 # from import_export.admin import ImportExportModelAdmin
-from import_export.admin  import ImportExportModelAdmin
+# from import_export.admin  import ImportExportModelAdmin
 
 # Register your models here.
 
-class UserColumns(ImportExportModelAdmin, admin.ModelAdmin):
+class UserColumns(admin.ModelAdmin):
     list_display = ["username", "email", "number", "password", "career", "location", "created"]
     
 
